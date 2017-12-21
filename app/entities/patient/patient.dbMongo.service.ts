e2deda2692af9687a82a4199c78f2e285b4ed61f
JulioJu <juanes0890@gmail.com>
2017-12-21 15:51:49 +0100

    Revert "Explanations of Promises (not functionnality added, some functionnalities breaked)"
    This reverts commit 3d30972624816e372186c7ad9d56878138839e3b.

--- app/entities/patient/patient.dbMongo.service.ts
+++ app/entities/patient/patient.dbMongo.service.ts
@@ -2,49 +2,6 @@ import { Patient } from './';
 
 let MongoClient = require('mongodb').MongoClient;
 
-const pseudoCondition = true;
-
-const fakeMongoInnerPromise = () => {
-    return new Promise((resolve, reject) => {
-        if (pseudoCondition) {
-            reject("Mongo est mort");
-        }
-        else {
-            setTimeout(() => {
-                console.log("n°4 coucou toujours exécuté from inner promise "
-                    + "(fake MongoDb)");
-                resolve("myReturnFromMongoDB");
-            }, 3000); // 4
-        }
-    });
-}
-
-export let outerPromise = () => {
-    return new Promise((resolve, reject) => {
-        fakeMongoInnerPromise()
-            .then((myVariable: string) => {
-                // divers traitement sur la variable qu'on a récupéré en back.
-                // Sinon cette promise sert à rien.
-                // Traitement qui prennent du temps, éventuellement
-                // Un super long : console.log
-                console.log("n°5: on récupère la variable du fake fakeMongoInnerPromise " +
-                    myVariable);
-                resolve(myVariable);
-            })
-            .catch(
-                (e: string)  => {
-                    console.log('On est dans le catch de l\'outer promise: '
-                        + e);
-                    reject(e);
-                }
-            );
-    });
-}
-
-export function nonnnnnPromise() {
-    setTimeout(() => console.log('n°3 coucouNonPromise'), 2000); // 3
-}
-
 export let dbAddPatient = function (myPatient: Patient) : Promise<any> {
     return new Promise ((resolve, reject) => {
         MongoClient.connect('mongodb://localhost/test', function(error, db){
