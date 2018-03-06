e09d93075fd1e97828548da3642f59c9202c6950
JulioJu <juanes0890@gmail.com>
2018-03-06 18:54:10 +0100

    Correct file tslint.yaml to retrieve the linter. Lint file ObserverTests.ts

--- back/app/ObserverTests.ts
+++ back/app/ObserverTests.ts
@@ -1,36 +1,38 @@
 
 // Some interesting examples to understand Promise and Observer mechamism
-function subscribe(observer: {next: (x:any)=>void, complete:()=>void}) {
-    var intervalID = setInterval(() => {
+function subscribe(observer: {next(x: any): void; complete(): void}) {
+// OR
+// function subscribe(observer: {next: (x:any) => void, complete:() => void}) {
+    const intervalID = setInterval(() => {
         observer.next('hi');
     }, 1000);
     console.log(observer);
 
-    return function unsubscribe() {
+    return () => {
         clearInterval(intervalID);
         observer.complete();
     };
 }
 
-var unsubscribe = subscribe({next: (x: any) => console.log(x),
+const unsubscribe = subscribe({next: (x: any) => console.log(x),
     complete: () => {
         console.info('done');
     }
 });
 
-
 // Later:
 setTimeout(() => unsubscribe(), 3000); // dispose the resources
 
-const myPromise = new Promise((res,rej) => {
+const myPromise = new Promise((res, rej) => {
     console.info('Corps promise');
     res('3');
-} )
+});
 // Promise is asynchrone
-myPromise.then((x) => {console.log('PromiseThen' + x);})
+myPromise.then((x) => {console.log('PromiseThen' + x)})
 const myFunc = (callback: any) => {
     console.info('Corps promise');
-    if (callback != null)
+    if (callback != null) {
         callback(3);
+    }
 }
 myFunc((x: any) => console.info(x));
