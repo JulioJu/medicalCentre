537024785a0bd1e32c38c8b508ac3c4f70c7f062
JulioJu <juanes0890@gmail.com>
2019-02-02 15:24:30 +0100

    Downgrade typscript (for compatibility with Angular) + remove non useful code in *.service.ts

--- front/src/app/entities/nurse/nurse.service.ts
+++ front/src/app/entities/nurse/nurse.service.ts
@@ -1,15 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient,
-    HttpResponse,
-    HttpErrorResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+    // HttpResponse,
+    // HttpErrorResponse } from '@angular/common/http';
+// import { Observable } from 'rxjs';
 import { SERVER_API_URL } from '../../app.constants';
 
-import { INurse } from
-    '../entities-interface/nurse.interface';
 import { AbstractService } from '../abstract';
-
-type EntityResponseType = HttpResponse<INurse> | HttpErrorResponse;
 
 @Injectable()
 // See my issue https://github.com/jhipster/generator-jhipster/issues/7302
@@ -17,22 +13,6 @@ export class NurseService extends AbstractService {
 
     public constructor(protected readonly http: HttpClient) {
         super(http, SERVER_API_URL + 'mongoose/nurses');
-    }
-
-    public insertOrUpdate(nurse: INurse): Observable<EntityResponseType> {
-        return super.insertOrUpdate(nurse) as Observable<EntityResponseType>;
-    }
-
-    public find(id: string): Observable<EntityResponseType> {
-        return super.find(id) as Observable<EntityResponseType>;
-    }
-
-    public query(req?: string): Observable<HttpResponse<INurse[]>> {
-        return super.query(req) as Observable<HttpResponse<INurse[]>>;
-    }
-
-    public delete(id: string): Observable<HttpResponse<INurse>> {
-        return super.delete(id) as Observable<HttpResponse<INurse>> ;
     }
 
 }
--- front/src/app/entities/patient/patient.service.ts
+++ front/src/app/entities/patient/patient.service.ts
@@ -1,15 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient,
-    HttpResponse,
-    HttpErrorResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { SERVER_API_URL } from '../../app.constants';
 
-import { IPatient } from
-    '../entities-interface/patient.interface';
 import { AbstractService } from '../abstract';
-
-type EntityResponseType = HttpResponse<IPatient> | HttpErrorResponse;
 
 @Injectable()
 // See my issue https://github.com/jhipster/generator-jhipster/issues/7302
@@ -17,22 +10,6 @@ export class PatientService extends AbstractService {
 
     public constructor(protected readonly http: HttpClient) {
         super(http, SERVER_API_URL + 'mongoose/patients');
-    }
-
-    public insertOrUpdate(patient: IPatient): Observable<EntityResponseType> {
-        return super.insertOrUpdate(patient) as Observable<EntityResponseType>;
-    }
-
-    public find(id: string): Observable<EntityResponseType> {
-        return super.find(id) as Observable<EntityResponseType>;
-    }
-
-    public query(req?: string): Observable<HttpResponse<IPatient[]>> {
-        return super.query(req) as Observable<HttpResponse<IPatient[]>>;
-    }
-
-    public delete(id: string): Observable<HttpResponse<IPatient>> {
-        return super.delete(id) as Observable<HttpResponse<IPatient>> ;
     }
 
 }
